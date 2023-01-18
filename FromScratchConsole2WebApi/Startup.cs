c0d4a2f36b2f169d5855faa2e8177aca5d9ecd2d
sagadavid <epostsaga@gmail.com>
2023-01-19 00:20:23 +0100

    commented version console2empty app

--- FromScratchConsole2WebApi/Startup.cs
+++ FromScratchConsole2WebApi/Startup.cs
@@ -12,14 +12,17 @@ namespace FromScratchConsole2WebApi
 
         //http request pipeline and environment
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) { 
-            app.UseRouting();
+            
+            app.UseRouting();//just enabling routing, not calling endpoint
+
             app.UseEndpoints(endpoints =>
             {
-
+                //mapget allows exact url as endpoint
                 endpoints.MapGet("/", async context =>
                 await context.Response.WriteAsync("console app to web api, experimental nishi ")
                 );
 
+                //a second endpoit for educational purposes
                 endpoints.MapGet("/newurl", async context =>
                 await context.Response.WriteAsync("new url new message")
                 );

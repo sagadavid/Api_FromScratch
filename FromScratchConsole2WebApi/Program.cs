73a8b28445e6d70940bd957ef5a0e2b862798759
sagadavid <epostsaga@gmail.com>
2023-01-18 17:03:59 +0100

    Add project files.

---
+++ FromScratchConsole2WebApi/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+
+namespace FromScratchConsole2WebApi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static IHostBuilder CreateHostBuilder() { }
+    }
+}

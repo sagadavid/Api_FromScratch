02feabb927512821cff53a4238193f025bf5f340
sagadavid <epostsaga@gmail.com>
2023-01-19 01:35:17 +0100

    addcontroller(), mapcontrollers(), testcontoller.cs, apicontroller attribute, get(), route attribute"",  route 2 methods -> to get errors app.usedeveloperexceptionpage(), route/action -> to get a mehtod among many

---
+++ FromScratchConsole2WebApi/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FromScratchConsole2WebApi.Controllers
+{
+    [ApiController]
+    //[Route("/")]//routes main
+    //[Route("test")]//routes test
+    [Route("test/[action]")]//reuires action method name for routing, unless 400
+    public class TestController:ControllerBase
+    {
+        public string Get() => "this is the resource to Get";
+
+        //we need to get error, of get1():AmbiguousMatchException..
+        //to get error add app.usedeveloperexceptionpage() under configure
+        public string Get1() => "this is the resource to Get1";
+    }
+}
--- FromScratchConsole2WebApi/Startup.cs
+++ FromScratchConsole2WebApi/Startup.cs
@@ -2,30 +2,45 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace FromScratchConsole2WebApi
 {
     public class Startup
     {
         //services
-        public void ConfigureServices(IServiceCollection services) { }
+        public void ConfigureServices(IServiceCollection services) 
+        { 
+            services.AddControllers();//only web api
+            //services.AddMvc();//mvc for api, for razor pages and for web app
+            //services.AddRazorPages();//only razorpages
+        
+        }
 
         //http request pipeline and environment
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) { 
-            
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) 
+        {
+            if (env.IsDevelopment()) { app.UseDeveloperExceptionPage(); }
+
             app.UseRouting();//just enabling routing, not calling endpoint
 
             app.UseEndpoints(endpoints =>
             {
-                //mapget allows exact url as endpoint
-                endpoints.MapGet("/", async context =>
-                await context.Response.WriteAsync("console app to web api, experimental nishi ")
-                );
+                //addcontrollers sevice requires own route mapping. 
+                //those below is empty template and self mapping. so, comment them.
+                //and add mapcontrollers.
 
-                //a second endpoit for educational purposes
-                endpoints.MapGet("/newurl", async context =>
-                await context.Response.WriteAsync("new url new message")
-                );
+                endpoints.MapControllers();
+
+                ////mapget allows exact url as endpoint
+                //endpoints.MapGet("/", async context =>
+                //await context.Response.WriteAsync("console app to web api, experimental nishi ")
+                //);
+
+                ////a second endpoit for educational purposes
+                //endpoints.MapGet("/newurl", async context =>
+                //await context.Response.WriteAsync("new url new message")
+                //);
             });
         }
     }

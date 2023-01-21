41713c7f3269c8148917d62c2b1fd3c5a37a51c1
sagadavid <epostsaga@gmail.com>
2023-01-21 13:24:57 +0100

    dynamic token replacement in routing and some route variations added

--- FromScratchConsole2WebApi/Controllers/ValuesController.cs
+++ FromScratchConsole2WebApi/Controllers/ValuesController.cs
@@ -10,6 +10,9 @@ namespace FromScratchConsole2WebApi.Controllers
     {
         [Route("api/getall")]
         [Route("getall")]
+        [Route("get-all")]//can have many url for one resourse,cant have one url  for many resources
+        [Route("values/getall")]//it works, tokenreplacement is dynamic version of this
+        [Route("[controller]/[action]")]//dynamic token replacement in routing
         public string GetAll()
         {
             return "return of getall action";
@@ -17,6 +20,7 @@ namespace FromScratchConsole2WebApi.Controllers
         
         [Route("api/getallauthors")]
         [Route("getallauthors")]
+        [Route("[controller]/[action]")]
         public string GetAllAuthors()
         { 
             return "return of getallauthors action"; 
@@ -27,17 +31,17 @@ namespace FromScratchConsole2WebApi.Controllers
         [Route("/{bookId}")]//variable in {}
         [Route("/book/{bookId}")]
         public string GetBookById(int bookId)
-        { return "you get \nbook#" + bookId; }
+        { return "you got \nbook#" + bookId; }
 
         [Route("/{bookId}/{citationId}")]
         [Route("/book/{bookId}/citation/{citationId}")]
-        public string GetCitedInBookById(int bookId, int citationId) 
+        public string GetCitationInBookById(int bookId, int citationId) 
         { return "you get \nbook#" + bookId + "\ncitation#" + citationId; }
 
         /* QUERY SEARCH IN ROUTE */
 
         [Route("search")]
-        [Route("kearch")]
+        [Route("kearch")]//even this works--> kearch? .. becasue of ?
         public string SearchBooks(int bookId, string bookName) 
         {
             return "seach result " +

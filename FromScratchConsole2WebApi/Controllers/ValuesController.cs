ece7ccbee9da51b3946436b04f58c460e462e0e6
sagadavid <epostsaga@gmail.com>
2023-01-21 13:51:31 +0100

    variable usage in token replacement in routing, overwrite by exceptional routing/discarding api level routing for any action added

--- FromScratchConsole2WebApi/Controllers/ValuesController.cs
+++ FromScratchConsole2WebApi/Controllers/ValuesController.cs
@@ -6,36 +6,42 @@ namespace FromScratchConsole2WebApi.Controllers
 {
     //[Route("api/[controller]")]
     [Route("[controller]/[action]")]//api level dynamic token replacement
+                                    //base url
     [ApiController]
     public class ValuesController : ControllerBase
     {
-       // [Route("api/getall")]
-        [Route("getall")]
-       // [Route("get-all")]//can have many url for one resourse,cant have one url  for many resources
+        // [Route("api/getall")]
+        //[Route("getall")]
+        // [Route("get-all")]//can have many url for one resourse,cant have one url  for many resources
         //[Route("values/getall")]//it works, tokenreplacement is dynamic version of this
-       // [Route("[controller]/[action]")]//dynamic token replacement in routing
+        // [Route("[controller]/[action]")]//dynamic token replacement in routing
         public string GetAll()
         {
             return "return of getall action";
         }
-        
-       // [Route("api/getallauthors")]
-        [Route("getallauthors")]
-       // [Route("[controller]/[action]")]
+
+        // [Route("api/getallauthors")]
+        //[Route("getallauthors")]
+        // [Route("[controller]/[action]")]
         public string GetAllAuthors()
-        { 
-            return "return of getallauthors action"; 
+        {
+            return "return of getallauthors action";
         }
 
         /* VARIABLES/DYNAMIC VALUES IN ROUTING */
 
         [Route("/{bookId}")]//variable in {}
         [Route("/book/{bookId}")]
+        [Route("{bookId}")]//completion of token replacement... only additional value is passed out of api level routing
+                           //-> type,fx: values/getbookbyid/34
+        [Route("~/book{bookId}")]//exceptional routing.. discards api level routing/base url for this action
         public string GetBookById(int bookId)
         { return "you got \nbook#" + bookId; }
 
         [Route("/{bookId}/{citationId}")]
         [Route("/book/{bookId}/citation/{citationId}")]
+        [Route("{bookId}:{citationId}")]//completion of token replacement
+                                        //can use : or , or / anything for seperation book-citation
         public string GetCitationInBookById(int bookId, int citationId) 
         { return "you get \nbook#" + bookId + "\ncitation#" + citationId; }
 

bbde2eee6104465ece4685635d708467a069c9f6
sagadavid <epostsaga@gmail.com>
2023-01-22 03:22:13 +0100

    type validation in routing against ambigious route call

---
+++ FromScratchConsole2WebApi/Controllers/BooksController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FromScratchConsole2WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [Route("{ident:int}")]//type validation in routing against ambigious route call
+        public string GetBookId(int ident)
+        { return "\nbookID# " + ident; }
+
+        [Route("{ident}")]
+        public string GetBookName(string ident)
+        { return "\nbookNAME# " + ident; }
+    }
+}

2777c3aaf84af3d352dae05fd24947e339f3f97d
sagadavid <epostsaga@gmail.com>
2023-01-23 00:25:05 +0100

    route constrains presented; type, min, max, minlength, maxlength, length, range, alpha, regex validation

--- FromScratchConsole2WebApi/Controllers/BooksController.cs
+++ FromScratchConsole2WebApi/Controllers/BooksController.cs
@@ -7,12 +7,24 @@ namespace FromScratchConsole2WebApi.Controllers
     [ApiController]
     public class BooksController : ControllerBase
     {
-        [Route("{ident:int}")]//type validation in routing against ambigious route call
+        //type validation in routing against ambigious route call
+        //:int and :min are route constrains
+        //int which are less than 10 is evaluated as string(next method )
+        //[Route("{ident:int:min(10):max(20)}")]
+        //[Route("{ident:int:range(10,15)")]
+        [Route("{ident:int}")]
         public string GetBookId(int ident)
         { return "\nbookID# " + ident; }
 
+        //[Route("{ident:length(3):alpha}")]//alpha means letters only, no other characters
+        //[Route("{ident:minlength(5):maxlength(8)}")]
         [Route("{ident}")]
         public string GetBookName(string ident)
         { return "\nbookNAME# " + ident; }
+
+        //check if regex is not conflicting with previous routes
+        //[Route("{ident:regex(a(b|f))}")]
+        //public string GetBookByRegex(string ident)
+        //{ return "\nregex bookNAME# " + ident; }
     }
-}
+} 

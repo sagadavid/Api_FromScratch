d3561a0d931461790ae340b7db063d29da18d156
sagadavid <epostsaga@gmail.com>
2023-01-26 21:27:12 +0100

    BindProperty in a controller

---
+++ FromScratchConsole2WebApi/Controllers/CountriesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FromScratchConsole2WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+
+        [BindProperty]
+        public string Name { get; set; }
+        
+        [BindProperty]
+        public int Population { get; set; }
+
+        [BindProperty]
+        public int Area { get; set; }
+
+        [HttpPost("")]
+        public IActionResult AddCountry()
+
+        //on api/countries-->postman/post/formdata{key,value-->name,norge}//200
+        
+            //{ return Ok(this.Name); }
+        { return Ok($"\nName={this.Name},\nPopulation={this.Population},\nArea={this.Area}"); }
+        
+
+    }
+}
